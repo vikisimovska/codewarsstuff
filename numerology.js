9a6c632f5cc06c9b3dc3ca33c4f51bc7efdce93f
ekong2 <eddiek93@gmail.com>
2015-06-12 21:03:03 -0700

    in progress: numerology

---
+++ numerology.js
@@ -0,0 +1,20 @@
+/**
+ * Your solution
+ * @date JS Date object
+ **/
+function solution(date){
+  console.log(date);
+  console.log(typeof date);
+  // var lib = "1234567890";
+  // var work = date.split('').filter(function(value){return (lib.indexOf(value) > -1)});
+  // var result = 0;
+  // work.map(function(value){
+  //   result += Number(value);
+  //   if (result > 10) {
+  //     result = 1 + (result%10);
+  //   }
+  // })
+  // return result;
+}
+
+// solution('06/14/2010')
