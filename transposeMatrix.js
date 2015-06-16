5eca17b61d8d7a0a9cac4470768969580ff4a0e3
ekong2 <eddiek93@gmail.com>
2015-06-15 19:04:25 -0700

    implemented shorter solution for transpose Matrix

--- transposeMatrix.js
+++ transposeMatrix.js
@@ -5,26 +5,13 @@ function transpose(matrix) {
   var rowSize = matrix.length; 
   var columnSize = matrix[0].length;
 
-  var direction = (direction)? direction : 1;
-
-  if (direction === 1){
-    for (var i = 0; i < columnSize; i++){
-      var currentRow = [];
-      for (var j = 0; j < rowSize; j++){
-        currentRow.push(matrix[j][i]);
-      }
-      result.push(currentRow);
-    }    
-  }
-  else{
-    for (var i = columnSize-1; i > -1; i--){
-      var currentRow = [];
-      for (var j = 0; j < rowSize; j++){
-        currentRow.push(matrix[j][i]);
-      }
-      result.push(currentRow);
-    } 
-  }
+  for (var i = 0; i < columnSize; i++){
+    var currentRow = [];
+    for (var j = 0; j < rowSize; j++){
+      currentRow.push(matrix[j][i]);
+    }
+    result.push(currentRow);
+  }    
 
   return result;
 }
