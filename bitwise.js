736a133acb8222fc3f23d06b783fed55d7ffc478
Ron Tsui <ronsuij@gmail.com>
2015-06-22 20:28:10 -0700

    implemented bitwise.js

--- bitwise.js
+++ bitwise.js
@@ -1,1 +1,33 @@
 //http://www.codewars.com/kata/523fba59cb9aaaef4f000135/train/javascript
+
+Number.prototype.twos = function(n) {
+  //You may assume for this excercise that  n >= 2... 
+  var work = this.toString(2);
+  var work2 = ""
+  var result;
+  for (var i = 0; i < work.length; i++){
+    if (work[i] === "-"){
+      work2 += "1";
+    }
+    else {
+      work2 += work[i];
+    }
+  }
+  if (work2.length >= n){
+    return work2.slice(work2.length-n, work2.length);
+  }
+  else {
+    if (work[0] === "-"){
+      while (work2.length < n){
+        work2 = "1" + work2;
+      }
+    }
+    else {
+      while (work2.length < n){
+        work2 = "0" + work2;
+      }
+    }
+    return work2;
+  }
+}
+
