4509fd22f4c8f536c9b35dc5c7a2feed33c6ea4c
vokoshyv <ronsuij@gmail.com>
2015-06-25 15:54:59 -0700

    created solution for explosiveSum.js that should work, but is timing out; must refactor

--- explosiveSum.js
+++ explosiveSum.js
@@ -1,5 +1,28 @@
 // http://www.codewars.com/kata/52ec24228a515e620b0005ef/train/javascript
 
 function sum(num) {
-  return 0;
+  console.log(num);
+  var work = [];
+  for (var i = 1; i < num+1; i++){
+    work.push(i);
+  }
+
+  var count = 0;
+
+  var recurse = function(currTotal, options){
+    if (currTotal === num){
+      count++;
+      return;
+    }
+    else if (currTotal > num){
+      return;
+    }
+
+    for (var i = 0; i < options.length; i++){
+      recurse(currTotal + options[i], options.slice(0, i+1))
+    }
+
+  }
+  recurse(0, work);
+  return count;
 }
