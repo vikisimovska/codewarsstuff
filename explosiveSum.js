5e019302e2859b8ba9dffbaa615a73e00f850f54
vokoshyv <ronsuij@gmail.com>
2015-06-25 15:37:49 -0700

    starting explosiveSum.js from scratch

--- explosiveSum.js
+++ explosiveSum.js
@@ -1,28 +1,5 @@
-//http://www.codewars.com/kata/52ec24228a515e620b0005ef/train/javascript
-
+// http://www.codewars.com/kata/52ec24228a515e620b0005ef/train/javascript
 
 function sum(num) {
-  var work = [];
-  for (var i = 1; i < num+1; i++){
-    work.push(i);
-  }
-  console.log(work);
-  var counter = 0;
-
-  var recurse = function(curr){
-  	if (curr === num){
-  		counter++;
-  		return;
-  	}
-  	else if (curr > num){
-  		return;
-  	}
-  	for (var i = work.length-1; i > -1; i--){
-  		recurse(curr + work[i]);
-  	}
-  }
-  recurse(0);
-
-  return counter;
-
+  return 0;
 }
