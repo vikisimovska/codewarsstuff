c266c6fba9b142b706f5ad4a06bc88be380d7740
vokoshyv <ronsuij@gmail.com>
2015-06-29 13:38:42 -0700

    initialized file for first 1kyu problem: tinyTHreePassCompiler.js

---
+++ tinyThreePassCompiler.js
@@ -0,0 +1,30 @@
+// http://www.codewars.com/kata/5265b0885fda8eac5900093b/train/javascript
+
+function Compiler () {};
+
+Compiler.prototype.compile = function (program) {
+  return this.pass3(this.pass2(this.pass1(program)));
+};
+
+Compiler.prototype.tokenize = function (program) {
+  // Turn a program string into an array of tokens.  Each token
+  // is either '[', ']', '(', ')', '+', '-', '*', '/', a variable
+  // name or a number (as a string)
+  var regex = /\s*([-+*/\(\)\[\]]|[A-Za-z]+|[0-9]+)\s*/g;
+  return program.replace(regex, ":$1").substring(1).split(':').map( function (tok) {
+    return isNaN(tok) ? tok : tok|0;
+  });
+};
+
+Compiler.prototype.pass1 = function (program) {
+  var tokens = this.tokenize(program);
+  // return un-optimized AST
+};
+
+Compiler.prototype.pass2 = function (ast) {
+  // return AST with constant expressions reduced
+};
+
+Compiler.prototype.pass3 = function (ast) {
+  // return assembly instructions
+};
