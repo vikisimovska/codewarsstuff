2ba2d36678564628bd13c8bdd1edaf90d3b98e20
vokoshyv <ronsuij@gmail.com>
2015-06-29 12:17:03 -0700

    implemented solution for vasyaClerk.js

--- vasyaClerk.js
+++ vasyaClerk.js
@@ -7,7 +7,44 @@ function tickets(peopleInLine){
   var check = true;
 
   for (var i = 0; i < peopleInLine.length; i++){
-    
+    if (peopleInLine[i] === 25){
+      bill25++;
+      continue;
+    }
+    else if (peopleInLine[i] === 50){
+      if (bill25 > 0){
+        bill50++;
+        bill25--;
+        continue;
+      }
+      else {
+        check = false;
+        break;
+      }
+    }
+    else if (peopleInLine[i] === 100){
+      if (bill50 === 0){
+        if (bill25 >= 3){
+          bill25 -= 3;
+          continue;
+        }
+        else {
+          check = false;
+          break;
+        }
+      }
+      else if (bill50 > 0){
+        if (bill25 > 0){
+          bill25 -= 1;
+          bill50 -= 1;
+          continue;
+        }
+        else {
+          check = false;
+          break;
+        }
+      }
+    }
   }
 
   return (check)? "YES" : "NO";
