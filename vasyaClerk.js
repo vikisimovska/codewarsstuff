3eab0d1140701524a28e0c7b70916229b3e51473
vokoshyv <ronsuij@gmail.com>
2015-06-29 12:09:10 -0700

    design for loop to go through people in line

--- vasyaClerk.js
+++ vasyaClerk.js
@@ -1,5 +1,14 @@
 // http://www.codewars.com/kata/555615a77ebc7c2c8a0000b8/train/javascript
 
 function tickets(peopleInLine){
-  // ...
+  var bill25 = 0;
+  var bill50 = 0;
+  var bill100 = 0;
+  var check = true;
+
+  for (var i = 0; i < peopleInLine.length; i++){
+    
+  }
+
+  return (check)? "YES" : "NO";
 }
