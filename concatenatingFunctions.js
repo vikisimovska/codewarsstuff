cfff5801f3c7ef347bb332d9fc27251167e6ed45
vokoshyv <ronsuij@gmail.com>
2015-06-29 13:39:08 -0700

    initialized file for concatenatingFunctions.js

---
+++ concatenatingFunctions.js
@@ -0,0 +1,7 @@
+// http://www.codewars.com/kata/527176c487961e5900000106/train/javascript
+
+// just a small amount of possible functions to start testing with.
+var addOne = function(e) {return e + 1;};
+var square = function(e) {return e * e;};
+
+// Extend the Function prototype with a method pipe
