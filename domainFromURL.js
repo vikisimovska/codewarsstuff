ceef3056f96f3900c095376a2fd63c22dc7fd2f4
vokoshyv <ronsuij@gmail.com>
2015-06-30 14:09:35 -0700

    cleaned up console logs from domainFromURL.js

--- domainFromURL.js
+++ domainFromURL.js
@@ -1,13 +1,11 @@
 // http://www.codewars.com/kata/514a024011ea4fb54200004b/train/javascript
 
 function domainName(url){
-  console.log(url);
   var work = url.split('.');
   if (url.indexOf('www') > -1){
     return work[1];
   }
   work = work[0];
-  console.log(work);
   if (work.indexOf('://') > -1){
     work = work.split('://');
     return work[1];
