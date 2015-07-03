d74703375543693b3218ebfee0043e84cbb13c60
vokoshyv <ronsuij@gmail.com>
2015-07-03 00:27:08 -0700

    implements solution for deltaBits.js

--- deltaBits.js
+++ deltaBits.js
@@ -1,8 +1,8 @@
 // http://www.codewars.com/kata/538948d4daea7dc4d200023f/train/javascript
 
-Number.prototype.twos = function(n) {
+var twos = function(n, input) {
   //You may assume for this excercise that  n >= 2... 
-  var work = this.toString(2);
+  var work = input.toString(2);
   var work2 = ""
   var result;
   for (var i = 0; i < work.length; i++){
@@ -29,4 +29,20 @@ Number.prototype.twos = function(n) {
     }
     return work2;
   }
+}
+
+function convertBits(a, b){
+
+  var workA = twos(50, a);
+  var workB = twos(50, b);
+
+  var counter = 0; 
+
+  for (var i = 0; i < workA.length; i++){
+    if (workA[i] !== workB[i]){
+      counter++;
+    }
+  }
+
+  return counter;
 }
