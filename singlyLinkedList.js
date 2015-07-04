1628b2302037602ee042b0c5c7076877dd48ed54
vokoshyv <ronsuij@gmail.com>
2015-07-04 14:57:20 -0700

    implements solution for singlyLinkedList.js

---
+++ singlyLinkedList.js
@@ -0,0 +1,28 @@
+// http://www.codewars.com/kata/527bbf9f8699010bf40006ee/train/javascript
+
+function List() {}
+
+function EmptyList() {}
+EmptyList.prototype = new List();
+EmptyList.prototype.constructor = EmptyList;
+
+EmptyList.prototype.toString = function() { /* implement this */ };
+EmptyList.prototype.isEmpty = function() { /* implement this */ };
+EmptyList.prototype.length = function() { /* implement this */ };
+EmptyList.prototype.push = function(x) { /* implement this */ };
+EmptyList.prototype.remove = function(x) { /* implement this */ };
+EmptyList.prototype.append = function(xs) { /* implement this */ };
+
+function ListNode(value, next) { /* implement this */ }
+ListNode.prototype = new List();
+ListNode.prototype.constructor = ListNode;
+ListNode.prototype.isEmpty = function() { /* implement this */ };
+
+ListNode.prototype.toString = function() { /* implement this */ };
+
+ListNode.prototype.head = function() { /* implement this */ };
+ListNode.prototype.tail = function() { /* implement this */  };
+ListNode.prototype.length = function() { /* implement this */ };
+ListNode.prototype.push = function(x) { /* implement this */ };
+ListNode.prototype.remove = function(x) { /* implement this */ };
+ListNode.prototype.append = function(xs) { /* implement this */ };
