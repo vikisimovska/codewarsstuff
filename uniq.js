ad304e0beac2e8dde86cdbfb04eb41a11542ff9d
vokoshyv <ronsuij@gmail.com>
2015-07-06 21:44:06 -0700

    implements solution for uniq.js

--- uniq.js
+++ uniq.js
@@ -1,4 +1,22 @@
 // http://www.codewars.com/kata/52250aca906b0c28f80003a1/train/javascript
 
+var input = ['a','a','b','b','c','a','b','c'];
+
 function uniq_c(a){
+  var prev = '';
+  var result = [];
+  var current = [];
+  for (var i = 0; i < a.length; i++){
+    if (a[i] !== prev){
+      result.push(current.slice());
+      prev = a[i];
+      current = [a[i], 1];
+    }
+    else {
+      current[1] = current[1] + 1;
+    }
+  }
+  result.push(current.slice());
+  result.shift();
+  return result;
 }
