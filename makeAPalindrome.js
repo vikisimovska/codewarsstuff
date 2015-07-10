5cdf7be3dd404ae0874a3b6541fcc6f454226469
vokoshyv <ronsuij@gmail.com>
2015-07-09 21:26:31 -0700

    made some progress on makeAPalindrome.js

--- makeAPalindrome.js
+++ makeAPalindrome.js
@@ -1,5 +1,17 @@
 // http://www.codewars.com/kata/53629117f81a7f9b9e000d1e/train/javascript
 
-function makePalindrome(text){
+var isPalindrome = function(input){
+  var work = '';
+  input.split('').map(function(letter){work = letter + work});
+  return (input === work)? true : false;
+}
+
+var addLetterToFront = function(){
   
+}
+
+
+function makePalindrome(text){
+  var work = text.slice();
+
 }
