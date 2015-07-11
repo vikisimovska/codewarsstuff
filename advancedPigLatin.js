fb36e6fb9017d435e2e5d05fc9e94979f293ef78
vokoshyv <ronsuij@gmail.com>
2015-07-10 17:44:04 -0700

    initializes file for advancedPigLatin.js

---
+++ advancedPigLatin.js
@@ -0,0 +1,7 @@
+// http://www.codewars.com/kata/533c46b140aafec05b000d31/train/javascript
+
+function translate(sentence) {
+  //Convert the sentence to Pig Latin!
+  // or should I say...
+  //Onvertcay ethay entencesay otay Igpay Atinlay!
+};
