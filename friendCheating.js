d019d43036dc8d1343c2f1df89c535ed85e0cb75
vokoshyv <ronsuij@gmail.com>
2015-07-13 15:40:16 -0700

    implements solution that doesn't work for large numbers

--- friendCheating.js
+++ friendCheating.js
@@ -1,4 +1,24 @@
 // http://www.codewars.com/kata/5547cc7dcad755e480000004/train/javascript
 
+var sumFromOneToN = function(n, remove1, remove2){
+  var work = 0;
+  for (var i = 1; i < n+1; i++){
+    if (i !== remove1 && i !== remove2){
+      work += i;
+    }
+  }
+  return work;
+}
+
 function removeNb (n) {
+  var work = [];
+  for (var i = 1; i < n; i++){
+    for (var j = i+1; j < n+1; j++){
+      if (sumFromOneToN(n, i, j) === i * j){
+        work.push([i, j]);
+        work.push([j, i]);
+      }
+    }
+  }
+  return work;
 }
