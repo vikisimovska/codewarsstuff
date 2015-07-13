00be18ea70c0fb4a82971d17b8cafa59c32612d2
vokoshyv <ronsuij@gmail.com>
2015-07-13 16:22:56 -0700

    implements solution to squareSequence.js

--- squaresSequence.js
+++ squaresSequence.js
@@ -1,5 +1,11 @@
 // http://www.codewars.com/kata/5546180ca783b6d2d5000062/train/javascript
 
 function squares(x, n) {
-  var work [];
+  var work = [x];
+  var temp;
+  while (work.length < n){
+    temp = work[work.length-1] * work[work.length-1];
+    work.push(temp);
+  }
+  return work;
 }
