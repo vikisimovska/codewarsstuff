31a7641fecff2835aa168ca09abafd73f3acbed6
vokoshyv <ronsuij@gmail.com>
2015-07-13 16:07:44 -0700

    implements solution for anagramsChecker.js

--- anagramsChecker.js
+++ anagramsChecker.js
@@ -1,5 +1,17 @@
 // http://www.codewars.com/kata/523a86aa4230ebb5420001e1/train/javascript
 
+var checkTwo = function(input1, input2){
+  var work1 = input1.split('').sort().join('');
+  var work2 = input2.split('').sort().join('');
+  return (work1 === work2)? true : false;
+}
+
 function anagrams(word, words) {
-  
+  var work = [];
+  words.map(function(toCompare){
+    if (checkTwo(word, toCompare)){
+      work.push(toCompare);
+    }
+  })
+  return work;
 }
