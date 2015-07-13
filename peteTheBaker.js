fe45bdd4f2bdc2788b5408cf1a6a5e9b6cd654d4
vokoshyv <ronsuij@gmail.com>
2015-07-13 15:27:45 -0700

    cleans up code for peteTheBaker.js

--- peteTheBaker.js
+++ peteTheBaker.js
@@ -1,20 +1,14 @@
 // http://www.codewars.com/kata/525c65e51bf619685c000059/train/javascript
 
 function cakes(recipe, available) {
-  console.log('RECIPE: ', recipe);
-  console.log('AVAILABLE: ', available);
   var necessaryItems = Object.keys(recipe);
   var availableItems = Object.keys(available);
   var howMany = [];
-  var workingOn = [];
   for (var i = 0; i < necessaryItems.length; i++){
     if (availableItems.indexOf(necessaryItems[i]) === -1){
       return 0;
     }
-    // workingOn.push(necessaryItems[i]):
     howMany.push(Math.floor(available[necessaryItems[i]] / recipe[necessaryItems[i]]));
   }
-  console.log(workingOn);
-  console.log(howMany);
   return Math.min.apply(this, howMany);
 }
