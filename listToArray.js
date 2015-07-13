6b7574172920bdb2c24abb50be7c5cc01efe753e
vokoshyv <ronsuij@gmail.com>
2015-07-13 16:43:52 -0700

    implements solution to listToArray.js

--- listToArray.js
+++ listToArray.js
@@ -1,5 +1,11 @@
 // http://www.codewars.com/kata/557dd2a061f099504a000088/train/javascript
 
 function listToArray(list) {
-  //write your code here
+  var current = list
+  var work = [];
+  while (current !== null){
+    work.push(current.value);
+    current = current.rest;
+  }
+  return work;
 }
