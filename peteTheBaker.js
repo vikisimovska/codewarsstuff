c7fc883753cbaa77a22243fd11a2077b6e89eb74
vokoshyv <ronsuij@gmail.com>
2015-07-13 15:05:48 -0700

    takes care of missing ingredients case for peteTheBaker.js

--- peteTheBaker.js
+++ peteTheBaker.js
@@ -1,5 +1,12 @@
 // http://www.codewars.com/kata/525c65e51bf619685c000059/train/javascript
 
 function cakes(recipe, available) {
-  
+  var necessaryItems = Object.keys(recipe);
+  var availableItems = Object.keys(available);
+
+  for (var i = 0; i < necessaryItems.length; i++){
+    if (availableItems.indexOf(necessaryItems[i]) === -1){
+      return 0;
+    }
+  }
 }
