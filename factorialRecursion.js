d14f7184f5f7b5211b373a2eebbde8f7169e99fd
vokoshyv <ronsuij@gmail.com>
2015-07-13 21:25:36 -0700

    still working on better solution for factorialRecursion.js

--- factorialRecursion.js
+++ factorialRecursion.js
@@ -1,5 +1,15 @@
 // http://www.codewars.com/kata/5500517fc50295fff2000423/train/javascript
 
 function factorial(num){
+  var work = 1;
 
+  var recurse = function(toMultiply){
+    if (toMultiply === 1){
+      return;
+    }
+    work *= toMultiply;
+    recurse(toMultiply-1);
+  }
+  recurse(num)
+  return work;
 }
