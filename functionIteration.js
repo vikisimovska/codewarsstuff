3f28a13e698e2068532671a0157deb364c28d680
vokoshyv <ronsuij@gmail.com>
2015-07-14 11:08:28 -0700

    initializes file for functionIteration.js

---
+++ functionIteration.js
@@ -0,0 +1,6 @@
+// http://www.codewars.com/kata/54b679eaac3d54e6ca0008c9/train/javascript
+
+var createIterator = function (func, n) {
+  // TODO: Write code here to return a function 
+  // that executes *func*, *n* times on a supplied input
+};
