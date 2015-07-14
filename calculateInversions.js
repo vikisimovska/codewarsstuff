75e1ee3510169e1a1965453e30f6ec3e4e8a0ae8
vokoshyv <ronsuij@gmail.com>
2015-07-13 21:50:20 -0700

    implements solution for calculateInersions.js

--- calculateInversions.js
+++ calculateInversions.js
@@ -1,31 +1,37 @@
-var i;
+// Calculate the number of inversions in array
+function countInversions( array ){
 
-// Feel free to add helper functions if needed.
-var changeLoc = function(array, index){
-  var a = array[index];
-  var b = array[index+1];
-  array[index] = b;
-  array[index+1] = a;
-  return array;
-}
+  var i;
 
+  // Feel free to add helper functions if needed.
+  var changeLoc = function(array, index){
+    var a = array[index];
+    var b = array[index+1];
+    array[index] = b;
+    array[index+1] = a;
+    return array;
+  }
 
-var bubbleSort = function(array) {
+  var count = 0;
 
-  for (i = array.length-1; i > 0; i--){
-    var check = 0; 
-    for (j = 0; j < i; j++){
-      if (array[j] > array[j+1]){
-        array = changeLoc(array, j);
-      }
-      else {
-        check++;
-      }
-      if (check === i){
-        console.log('took shortcut');
-        return array;
+  var bubbleSort = function(array) {
+
+    for (i = array.length-1; i > 0; i--){
+      var check = 0; 
+      for (j = 0; j < i; j++){
+        if (array[j] > array[j+1]){
+          count++;
+          array = changeLoc(array, j);
+        }
+        else {
+          check++;
+        }
       }
     }
-  }
-  return array;
-};
+  };
+
+  bubbleSort(array);
+  
+  return count;
+
+}
