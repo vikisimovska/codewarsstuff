ec2f7550a50e57bbb7ec19a90498b5e9c8a6cc1a
vokoshyv <ronsuij@gmail.com>
2015-07-13 19:56:34 -0700

    implements solution for longestSequenceZero.js, but it takes too long to run for longer arrays

--- longestSequenceZero.js
+++ longestSequenceZero.js
@@ -1,5 +1,18 @@
 // http://www.codewars.com/kata/52b4d1be990d6a2dac0002ab/train/javascript
 
+var sumArrayValues = function(arr){
+  return arr.reduce(function(prev, curr){return prev + curr});
+}
+
 var maxZeroSequence = function(arr) {
-  // write your magic here
+  console.log(arr);
+  var work = [];
+  for (var i = 0; i < arr.length-1; i++){
+    for (var j = i+2; j < arr.length+1; j++){
+      if (sumArrayValues(arr.slice(i, j)) === 0 && arr.slice(i, j).length > work.length){
+        work = arr.slice(i, j);
+      }
+    }
+  }
+  return work;
 }
