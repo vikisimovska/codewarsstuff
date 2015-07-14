662f05d88b533720cdfdbde9762c6957dd40c9bc
vokoshyv <ronsuij@gmail.com>
2015-07-14 11:17:13 -0700

    still work to be done on functionIteration.js

--- functionIteration.js
+++ functionIteration.js
@@ -3,4 +3,17 @@
 var createIterator = function (func, n) {
   // TODO: Write code here to return a function 
   // that executes *func*, *n* times on a supplied input
+  var previous;
+
+  return function(){
+    if (previous !== undefined){
+      var toRun = arguments[0];
+      previous = func.apply(this, toRun);
+      return previous;
+    }
+    else {
+      previous = func.apply(this, [previous]);
+      return previous;
+    }
+  }
 };
