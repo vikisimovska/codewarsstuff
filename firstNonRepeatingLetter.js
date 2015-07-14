bd431fd424da5288eb49df7da6c146102fdf2de0
vokoshyv <ronsuij@gmail.com>
2015-07-13 20:51:03 -0700

    implements solution for firstNonRepeatingLetter.js

--- firstNonRepeatingLetter.js
+++ firstNonRepeatingLetter.js
@@ -1,17 +1,20 @@
 // http://www.codewars.com/kata/52bc74d4ac05d0945d00054e/train/javascript
 
 function firstNonRepeatingLetter(s) {
-  for (var i = 0; i < s.length-1; i++){
-    var count = 0;
-    for (var j = i+1; j < s.length; j++){
-      var temp = s[i];
-      if (s[i].toLowerCase() === s[j].toLowerCase()){
-        count++;
-      }
+  var work = {};
+
+  for (var i = 0; i < s.length; i++){
+    if (work[s[i].toLowerCase()]){
+      work[s[i].toLowerCase()][1] += 1;
     }
-    if (count === 0){
-      return temp;
+    else {
+      work[s[i].toLowerCase()] = [s[i], 1];
     }
   }
+  for (var key in work){
+    if (work[key][1] === 1){
+      return work[key][0];
+    }
+  }
+  return ''
 }
-
