4dacc21aedb2425b8a298ad5acae60698af26014
vokoshyv <ronsuij@gmail.com>
2015-07-13 17:47:27 -0700

    implements solution to vendingMachine.js

--- vendingMachine.js
+++ vendingMachine.js
@@ -2,14 +2,20 @@
 
 function findSpecialIdx(vms) {
   // FIXME: This isn't a solution :(
-  var prediction = 100;
-  vms[0].vend();
-  vms[3].vend();
-  vms[3].vend();
-  console.log(vms.weigh());
-  //for (var i = 0 ; i < 100 ; i++) {
-  //  vms[i].vend();
-  //  if (vms.weigh() != prediction) return i;
-  //  else prediction += 100;
-  //}
+  // var prediction = 100;
+  // vms[0].vend();
+  // vms[3].vend();
+  // vms[3].vend();
+  var count;
+
+  for (var i = 0; i < 100; i++){
+    count = 0; 
+    while (count < i+1){
+      vms[i].vend();
+      count++;
+    }
+  }
+  var work = vms.weigh().toString();
+  work = Number(work[work.length-3] + work[work.length-2] + work[work.length-1]) - 1;
+  return work;
 };
