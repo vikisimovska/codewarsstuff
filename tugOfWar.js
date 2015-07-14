0e046fbf28e7d1cf843a7a53da1a86c9f9d2e3f0
vokoshyv <ronsuij@gmail.com>
2015-07-13 17:24:04 -0700

    implements solution for tugOfWar.js

--- tugOfWar.js
+++ tugOfWar.js
@@ -1,5 +1,28 @@
 // http://www.codewars.com/kata/547fb94931cce5f5de00024f/train/javascript
 
+Array.prototype.sumItUp = function(){
+  return this.reduce(function(prev, curr){return prev + curr});
+}
+
 function tug_o_war(teams) {
-  //Code
+  console.log(teams);
+  var team1Sum = teams[0].sumItUp();
+  var team2Sum = teams[1].sumItUp();
+  if (team1Sum > team2Sum){
+    return 'Team 1 wins!';
+  }
+  else if (team2Sum > team1Sum){
+    return 'Team 2 wins!';
+  }
+  else {
+    if (teams[0][0] > teams[1][teams[0].length-1]){
+      return 'Team 1 wins!';
+    }
+    else if (teams[0][0] < teams[1][teams[0].length-1]){
+      return 'Team 2 wins!';
+    }
+    else {
+      return "It's a tie!";
+    }
+  }
 }
