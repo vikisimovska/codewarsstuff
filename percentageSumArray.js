aec581a08154d75f2af2dc7b1156fe5e86eda226
vokoshyv <ronsuij@gmail.com>
2015-07-13 21:41:03 -0700

    hmm, problem for percentageSumArray.js is funky

--- percentageSumArray.js
+++ percentageSumArray.js
@@ -1,7 +1,12 @@
 // http://www.codewars.com/kata/55142efb1d1eb59512000002/train/javascript
 
 function percentageSumArray(arr){
-
-// Input yor magic.
-
+  var percentage = arr.length; 
+  if (percentage === 0){
+    return false;
+  }
+  percentage = percentage / 100;
+  var sumOfArray = arr.reduce(function(prev, curr){return prev + curr});
+  var result = percentage * sumOfArray;
+  return result.toFixed(2);
 }
