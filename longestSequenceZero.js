a226c5f7e0987a593b0bf47ddc7dc1173c71cfd5
vokoshyv <ronsuij@gmail.com>
2015-07-13 19:16:00 -0700

    initializes file for longestSequenceZero.js

---
+++ longestSequenceZero.js
@@ -0,0 +1,5 @@
+// http://www.codewars.com/kata/52b4d1be990d6a2dac0002ab/train/javascript
+
+var maxZeroSequence = function(arr) {
+  // write your magic here
+}
--- vendingMachine.js
+++ vendingMachine.js
@@ -1,13 +1,7 @@
 // http://www.codewars.com/kata/526b1dd476952c04cf0000ab/train/javascript
 
 function findSpecialIdx(vms) {
-  // FIXME: This isn't a solution :(
-  // var prediction = 100;
-  // vms[0].vend();
-  // vms[3].vend();
-  // vms[3].vend();
   var count;
-
   for (var i = 0; i < 100; i++){
     count = 0; 
     while (count < i+1){
@@ -16,6 +10,5 @@ function findSpecialIdx(vms) {
     }
   }
   var work = vms.weigh().toString();
-  work = Number(work[work.length-3] + work[work.length-2] + work[work.length-1]) - 1;
-  return work;
+  return Number(work[work.length-3] + work[work.length-2] + work[work.length-1]) - 1;
 };
