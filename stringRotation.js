be46232503cb67be2522f55ac1b84813f5bc74ec
vokoshyv <ronsuij@gmail.com>
2015-07-14 11:49:56 -0700

    implements solution for stringRotation.js

--- stringRotation.js
+++ stringRotation.js
@@ -1,5 +1,20 @@
 // http://www.codewars.com/kata/5596f6e9529e9ab6fb000014/train/javascript
 
+var moveBack = function(input){
+  input = input.split('');
+  var result = input.slice(1).concat(input.slice(0, 1));
+  return result.join('');;
+}
+
 function shiftedDiff(first,second){
-  
+  var limit = second.length;
+  var count = 0;
+  while (first !== second){
+    second = moveBack(second);
+    count++;
+    if (count > limit){
+      return -1;
+    }
+  }
+  return count;
 }
