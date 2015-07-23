49aea36ec93b4bedef2dd1932746dad9c78ea6b4
vokoshyv <ronsuij@gmail.com>
2015-07-23 13:57:39 -0700

    initializes file for pointInPolygon.js

---
+++ pointInPolygon.js
@@ -0,0 +1,7 @@
+// http://www.codewars.com/kata/530265044b7e23379d00076a/train/javascript
+
+//Return true if point is inside poly, and false if it is not
+function pointInPoly(poly, point) {
+  return false;
+}
+
