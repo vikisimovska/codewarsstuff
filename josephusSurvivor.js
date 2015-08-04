ffc032caa348c2df5a3d095d9da800ddd7e68eb3
vokoshyv <ronsuij@gmail.com>
2015-08-04 16:03:40 -0700

    implements solution for josephusSurvivor.js

--- josephusSurvivor.js
+++ josephusSurvivor.js
@@ -1,3 +1,16 @@
 function josephusSurvivor(n,k){
-  
+  var work = [];
+  for (var i = 1; i < n+1; i++){
+    work.push(i);
+  }
+
+  var pointer = 0;
+
+  while (work.length > 1){
+    pointer = (pointer + k - 1) % work.length; 
+    work.splice(pointer, 1);
+  }
+  return work[0];
 }
+
+josephusSurvivor(7, 3);
