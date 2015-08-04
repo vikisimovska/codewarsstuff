4a762f3e634b39a340b4f075ebda2c5a9b8f4977
vokoshyv <ronsuij@gmail.com>
2015-08-04 15:42:16 -0700

    found another solution that was not much faster for leastCommonMultiple.js

--- leastCommonMultiple.js
+++ leastCommonMultiple.js
@@ -1,15 +1,16 @@
 var lcm = function () {
-  var checkAgainst = Math.max.apply(this, arguments) - 1;
+  var checkAgainst = Math.max.apply(this, arguments);
   var check = true;
+  var toAdd = checkAgainst;
 
   while (check){
     check = false;
-    checkAgainst++;
     for (var i = 0; i < arguments.length; i++){
       if (checkAgainst % arguments[i] !== 0){
         check = true;
       }
     }
+    checkAgainst += toAdd;
   }
-  return checkAgainst;
+  return checkAgainst - toAdd;
 };
