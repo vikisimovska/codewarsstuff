c78a5dbbe91ef59c60bea0e0b534af819346bad9
Ron Tsui <ronsuij@gmail.com>
2015-08-16 20:41:27 -0700

    implements solution for meanSquareError.js

--- meanSquareError.js
+++ meanSquareError.js
@@ -1,3 +1,7 @@
 var solution = function(firstArray, secondArray) {
-  
+  var work = 0;
+  firstArray.forEach(function(value, index){
+    work += Math.abs((value - secondArray[index]) * (value - secondArray[index]));
+  })
+  return work / firstArray.length;
 }
