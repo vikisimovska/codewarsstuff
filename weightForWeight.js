d12fe3d0f52e5d96906495751d575066d8c77fe7
vokoshyv <ronsuij@gmail.com>
2015-08-17 11:22:22 -0700

    implements solution, but codewars servers are funky right now; will resubmit later for weightForWeight.js

--- weightForWeight.js
+++ weightForWeight.js
@@ -1,3 +1,23 @@
-function orderWeight(strng) {
-    // your code
+function orderWeight(input) {
+  var work = input.split(' ').map(function(value){return Number(value)});
+  var check;
+
+  var compareWaz = function(a, b){
+    var aSplitUp = a.toString().split('').reduce(function(prev, curr){return Number(prev) + Number(curr)});
+    var bSplitUp = b.toString().split('').reduce(function(prev, curr){return Number(prev) + Number(curr)});
+
+    if (aSplitUp < bSplitUp){
+      return -1;
+    }else if (aSplitUp > bSplitUp){
+      return 1;
+    }else {
+      return 0;
+    }
+  }
+
+  work.sort(function(a, b){
+    return compareWaz(a, b);
+  })
+
+  return work;
 }
