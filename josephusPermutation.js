55ee8d616e32a3198257864e7e079d8f1753f141
vokoshyv <ronsuij@gmail.com>
2015-08-21 09:32:55 -0700

    implements solution for josephusPermutation.js

--- josephusPermutation.js
+++ josephusPermutation.js
@@ -1,3 +1,16 @@
 function josephus(items,k){
-  //your code here
+  var pointer = 0;
+
+  var work = [];
+
+  while (items.length > 0){
+    pointer += (k-1);
+    if (pointer >= items.length){
+      pointer = pointer % items.length;
+    }
+    var temp = items.splice(pointer, 1)[0];
+    work.push(temp);
+  }
+
+  return work;
 }
