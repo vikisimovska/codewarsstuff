6b83ed131be26faa1bb97815d97c9f3c831d8738
Ron Tsui <ronsuij@gmail.com>
2015-08-22 22:40:31 -0700

    did some work with bitCalc.js

--- bitCalc.js
+++ bitCalc.js
@@ -1,3 +1,9 @@
 function calculate(num1,num2){
+  function bin2dec(num){
+    return num.split('').reverse().reduce(function(x, y, i){
+      return (y === '1') ? x + Math.pow(2, i) : x;
+    }, 0);
+  }
 
+  return bin2dec(num1) + bin2dec(num2);
 }
