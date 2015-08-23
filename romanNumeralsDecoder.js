45c33ae54ddb9f141aca84f1069692c7da277acb
Ron Tsui <ronsuij@gmail.com>
2015-08-22 22:50:54 -0700

    implements solution for romanNumeralsDecoder.js

--- romanNumeralsDecoder.js
+++ romanNumeralsDecoder.js
@@ -1,4 +1,24 @@
 function solution(roman){
-  // complete the solution by transforming the 
-  // string roman numeral into an integer  
+  
+  var lib = {
+    'M': 1000,
+    'D': 500,
+    'C': 100, 
+    'L': 50, 
+    'X': 10, 
+    'V': 5, 
+    'I': 1
+  };
+
+  var count = 0;
+
+  for (var i = 0; i < roman.length; i++){
+    if (lib[roman[i+1]] !== undefined && lib[roman[i]] < lib[roman[i+1]]){
+      count -= lib[roman[i]];
+    }else {
+      count += lib[roman[i]];
+    }
+  }
+
+  return count;
 }
