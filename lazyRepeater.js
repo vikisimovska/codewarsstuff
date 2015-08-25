b2990bcf5d9a6a00c6c29e609ac99d62cb4ece97
vokoshyv <ronsuij@gmail.com>
2015-08-25 15:21:04 -0700

    starting lazyRepeater.js from scratch

--- lazyRepeater.js
+++ lazyRepeater.js
@@ -1,12 +1,3 @@
-// http://www.codewars.com/kata/51fc3beb41ecc97ee20000c3/train/javascript
-
 function makeLooper(str) {
   // TODO: return a function that loops through 'str' on successive invocations
-  this.work = str;
-  this.workLength = str.length;
-  this.counter = (this.counter)? this.counter : 0
-
-  this.counter++;
-  return this.work[counter % this.workLength];
 }
-
