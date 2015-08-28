1655874d52087eab25ccda4bbea86a74d9b198f6
vokoshyv <ronsuij@gmail.com>
2015-08-28 15:31:10 -0700

    implements solution for humanRand.js

--- humanRand.js
+++ humanRand.js
@@ -1,8 +1,17 @@
 function RNG(n) {
   this.n = n;  
+  this.store = [];
 }
 
 RNG.prototype.rand = function() {
   //return next random number
-  
+  if (this.store.length === this.n){
+    this.store = [];
+  }
+  var temp = Math.floor(Math.random() * this.n);
+  while (this.store.indexOf(temp) > -1){
+    temp = Math.floor(Math.random() * this.n);
+  }
+  this.store.push(temp);
+  return temp;
 }
