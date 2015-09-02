2e87750b928172e3abcf543830180c9012a44588
Ron Tsui <ronsuij@gmail.com>
2015-09-02 15:32:33 -0700

    implements solution for vectorClass.js

--- vectorClass.js
+++ vectorClass.js
@@ -1,3 +1,83 @@
 var Vector = function (components) {
-  // TODO: Finish the Vector class.
+  this.storage = components;
 };
+
+Vector.prototype.add = function(toAdd){
+  if (this.storage.length !== toAdd.storage.length){
+    throw "Error"
+    return;
+  }
+
+  var result = [];
+
+  for (var i = 0; i < this.storage.length; i++){
+    result.push(this.storage[i] + toAdd.storage[i]);
+  }
+
+  return result;
+}
+
+Vector.prototype.subtract = function(toSubtract){
+  if (this.storage.length !== toSubtract.storage.length){
+    throw "Error"
+    return;
+  }
+
+  var result = [];
+
+  for (var i = 0; i < this.storage.length; i++){
+    result.push(this.storage[i] - toSubtract.storage[i]);
+  }
+
+  return result;
+}
+
+Vector.prototype.dot = function(toDot){
+  if (this.storage.length !== toDot.storage.length){
+    throw "Error"
+    return;
+  }
+
+  var result = 0
+
+  for (var i = 0; i < this.storage.length; i++){
+    result += this.storage[i] * toDot.storage[i];
+  }
+
+  return result;
+}
+
+Vector.prototype.norm = function(){
+
+  var work = 0
+
+  for (var i = 0; i < this.storage.length; i++){
+    work += Math.pow(this.storage[i], 2);
+  }
+
+  return Math.sqrt(work);;
+}
+
+Vector.prototype.toString = function(){
+  return "(" + this.storage.join(',') + ")";
+}
+
+Vector.prototype.equals = function(toCheck){
+
+  for (var i = 0; i < this.storage.length; i++){
+    if (this.storage[i] !== toCheck.storage[i]){
+      return false;
+    }
+  }
+  return true;
+}
+
+Array.prototype.equals = function(toCheck){
+
+  for (var i = 0; i < this.length; i++){
+    if (this[i] !== toCheck.storage[i]){
+      return false;
+    }
+  }
+  return true;
+}
