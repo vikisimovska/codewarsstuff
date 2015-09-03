fd9fa13b2fc401e9eef679a313efd7c49529dae0
vokoshyv <ronsuij@gmail.com>
2015-09-03 14:27:55 -0700

    works somemore on prefillArray.js, tricky little bugger

--- prefillArray.js
+++ prefillArray.js
@@ -1,6 +1,27 @@
 function prefill(n, v) {
   if (typeof n !== 'number'){
-    throw TypeError
+    throw TypeError;
+  } else if (n % 1 !== 0){
+  	throw TypeError;
+  } else if (n === Number.POSITIVE_INFINITY || n === Number.NEGATIVE_INFINITY){
+  	console.error("Infinity is invalid");
+  	return;
+  } else if (n === true){
+  	console.log('reached here');
+  	throw "true is invalid";
   }
-  return Array(n+1).join(v + 'blah').split('blah');
+
+  var result = [];
+
+  var recurse = function(depth){
+  	if (depth === n){
+  		return;
+  	}
+  	result.push(v);
+
+  	recurse(depth + 1);
+  }
+  recurse(0)
+
+  return result;
 }
