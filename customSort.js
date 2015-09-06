b98e652f11fe0ba58a6f0a9af0a0ec57ae5a6cd0
Ron Tsui <ronsuij@gmail.com>
2015-09-06 13:44:46 -0700

    customSort in progress

--- customSort.js
+++ customSort.js
@@ -1,3 +1,12 @@
 function sort(items){
-  // create your custom sort logic 
+  var work = [];
+
+  for (var i = 0; i < items.length; i++){
+    work.push([items[i]]);
+  }
+
+  while (work.length !== 1){
+    var temp = [];
+    
+  }
 }
