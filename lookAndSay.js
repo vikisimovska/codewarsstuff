06f37766667d70bfc9c11991b75141d87d330ee4
vokoshyv <ronsuij@gmail.com>
2015-09-12 14:46:40 -0700

    implements solution, but it takes too long to run

--- lookAndSay.js
+++ lookAndSay.js
@@ -1,3 +1,30 @@
+var makeNext = function(input){
+  var result = '';
+  var prev = input[0];
+  var count = 1;
+
+  for (var i = 1; i < input.length; i++){
+    if (input[i] !== prev){
+      result += count.toString();
+      result += prev.toString();
+      prev = input[i];
+      count = 1;
+    } else {
+      count++;
+    }
+  }
+  result += count.toString();
+  result += prev.toString();
+  return result;
+}
+
 function getLines(line){
-  // code me please?
+  var results = ['1'];
+  line--;
+
+  while (line--){
+    results.push(makeNext(results[results.length-1]));
+  }
+
+  return results.join(',');
 }
