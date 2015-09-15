e237bd98f8e9bbfa7b1f7072c9158202bd7ae86e
vokoshyv <ronsuij@gmail.com>
2015-09-14 19:59:50 -0700

    adds url for reference

--- whitespaceInterpreter.js
+++ whitespaceInterpreter.js
@@ -1,3 +1,5 @@
+// http://www.codewars.com/kata/52dc4688eca89d0f820004c6/train/javascript
+
 // to help with debugging
 function unbleach (n) {
   if (n) return n.replace(/ /g, 's').replace(/\t/g, 't').replace(/\n/g, 'n');
