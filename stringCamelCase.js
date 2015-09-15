3a892546e98142d0a4658a6994b84c2d3715105b
vokoshyv <ronsuij@gmail.com>
2015-09-14 19:51:38 -0700

    implements solution for stringCamelCase.js

--- stringCamelCase.js
+++ stringCamelCase.js
@@ -1,3 +1,24 @@
-function toCamelCase(str){
-
+var makeCamel = function(word){
+  word = word.toLowerCase();
+  word = word[0].toUpperCase() + word.slice(1);
+  console.log(word);
+  return word;
 }
+
+function toCamelCase(str){
+  var work = (str.indexOf('-') > -1)? str.split('-') : str.split('_');
+
+  work = work.map(function(word, index){
+    if (index === 0){
+      return word;
+    }
+    return makeCamel(word);
+
+  })
+  console.log(work.join(''));
+  return work.join('');
+}
+
+
+// toCamelCase('');
+toCamelCase('the_stealth_warrior');
