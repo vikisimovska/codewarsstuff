32b37bb60c6778eb17feb8bb187cf9e455d31a13
Ron Tsui <ronsuij@gmail.com>
2015-09-26 19:13:02 -0700

    implements solution for highestAndLowest.js

--- highestAndLowest.js
+++ highestAndLowest.js
@@ -1,3 +1,3 @@
 function highAndLow(numbers){
-  // ...
+  return (Math.max.apply(this, numbers.split(' ').map(function(value){return Number(value)}))).toString() + ' ' + (Math.min.apply(this, numbers.split(' ').map(function(value){return Number(value)}))).toString()
 }
