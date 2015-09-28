2f011e1de9f7db370bd5930458e01ae4fcc45fbc
vokoshyv <ronsuij@gmail.com>
2015-09-28 11:05:28 -0700

    implements solution for flattenNestedMap.js

--- flattenNestedMap.js
+++ flattenNestedMap.js
@@ -1,4 +1,32 @@
 // Flattens an hierarchical map into a single level
 function flattenMap(map) {
-  // Add your code here
+  var result = {};
+
+  var traverse = function(keyBuiltUp, takeFrom){
+    if (!(typeof takeFrom === "object" && takeFrom !== null && !Array.isArray(takeFrom))){
+      result[keyBuiltUp.join('/')] = takeFrom;
+      return;
+    }
+
+    for (var key in takeFrom){
+      keyBuiltUp.push(key);
+      traverse(keyBuiltUp, takeFrom[key]);
+      keyBuiltUp.pop();
+    }
+  }
+
+  traverse([], map);
+  return result;
 }
+
+var test = {
+  'a': {
+    'b': {
+      'c': 12,
+      'd': 'Hello World'
+    },
+    'e': [1,2,3]
+  }
+};
+
+flattenMap(test);
