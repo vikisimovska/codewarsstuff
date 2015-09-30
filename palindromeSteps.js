29fd4c17aa70256de0e27efe10c54047d569972c
vokoshyv <ronsuij@gmail.com>
2015-09-30 11:31:36 -0700

    implements solution for palindromeSteps.js

--- palindromeSteps.js
+++ palindromeSteps.js
@@ -1,3 +1,22 @@
-var palindromeChainLength = function(n) {
+var giveReverseNumber = function(input){
+  var work = input.toString();
+  var i = work.length; 
+  var result = '';
+  while (i--){
+    result += work[i];
+  }
+  return Number(result);
+}
 
+var palindromeChainLength = function(n) {
+  var counter = 0;
+  var backwards = giveReverseNumber(n);
+
+  while (n !== backwards){
+    counter++;
+    n = backwards + n;
+    backwards = giveReverseNumber(n);
+  }
+
+  return counter;
 };
