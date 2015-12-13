3891e6583bc27e48e7b4c1389f0df873fa1a4991
Ron Tsui <ronsuij@gmail.com>
2015-12-12 21:12:24 -0800

    implements solution for alphaNum.js

--- alphaNum.js
+++ alphaNum.js
@@ -1,3 +1,13 @@
 function alphanumeric(string){
-  //your code here
+  var dict = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+  var check = true;
+
+  string.split('').forEach(function(char){
+    if (dict.indexOf(char) === -1){
+      check = false;
+      return;
+    }
+  })
+
+  return string.length > 0? check : false;
 }
