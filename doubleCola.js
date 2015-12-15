26ebe219c5b5a84d9a913edf0a45ebb8b3a1e296
vokoshyv <ronsuij@gmail.com>
2015-12-14 16:31:45 -0800

    implements solution, but tests may be funky

--- doubleCola.js
+++ doubleCola.js
@@ -1,3 +1,9 @@
-function whoIsNext(names, r){
-  //your code here
+function whoIsNext(names, r){  
+  var pointer = 0;
+  while (names[r-1] === undefined){
+    names.push(names[pointer]);
+    names.push(names[pointer]);
+    pointer++;
+  }
+  return names[r-1];
 }
