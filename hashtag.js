ad72bf9b2469381a51c75919c0c6c6776ba73f8d
Ron Tsui <ronsuij@gmail.com>
2015-12-28 17:25:20 -0800

    implements solution for hashtag.js

--- hashtag.js
+++ hashtag.js
@@ -1,3 +1,23 @@
 function getHashtags(post) {
-  // Add logic here
+  console.log(post);
+  
+  var work = [];
+  var lib = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  post.split(' ').forEach(function(word){
+    if (word[0] === '#'){
+      var justWord;
+      for (var i = 0; i < word.length; i++){
+        if (word[i] !== '#'){
+          justWord = word.slice(i);
+          break;
+        }
+      }
+
+      if (justWord !== undefined && justWord.split('#').length ===1 && lib.indexOf(justWord[0]) > -1){
+        work.push(justWord);
+      }
+    }
+  })
+  return work;
 }
