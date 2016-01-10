196c80e087a13b2bec42de1b78cadcfda3ef526a
Ron Tsui <ronsuij@gmail.com>
2016-01-09 16:56:29 -0800

    cleans up rangeExtraction.js

--- rangeExtraction.js
+++ rangeExtraction.js
@@ -6,8 +6,6 @@ function solution(list){
     if (list[i] !== list[i-1]+1){
       work.push([start, list[i-1]]);
       start = list[i];
-    } else {
-
     }
   }
   work.push([start, list[list.length-1]]);
@@ -23,7 +21,6 @@ function solution(list){
       result.push(chunk[0].toString() + '-' + chunk[1].toString());
     }
   })
-  console.log(work);
-}
 
-solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]);
+  return result.join(',');
+}
