c4ae43918f941d8dbc615b2361ebc47fc2b0533f
Ron Tsui <ronsuij@gmail.com>
2016-01-09 16:55:21 -0800

    implements solution for raneExtraction.js

--- rangeExtraction.js
+++ rangeExtraction.js
@@ -1,3 +1,29 @@
 function solution(list){
- // TODO: complete solution 
+  var work = [];
+  var start = list[0];
+
+  for (var i = 1; i < list.length; i++){
+    if (list[i] !== list[i-1]+1){
+      work.push([start, list[i-1]]);
+      start = list[i];
+    } else {
+
+    }
+  }
+  work.push([start, list[list.length-1]]);
+  var result = [];
+  
+  work.forEach(function(chunk){
+    if (chunk[0] === chunk[1]){
+      result.push(chunk[0].toString());
+    } else if (chunk[1] - chunk[0] < 2){
+      result.push(chunk[0].toString());
+      result.push(chunk[1].toString());
+    } else{
+      result.push(chunk[0].toString() + '-' + chunk[1].toString());
+    }
+  })
+  console.log(work);
 }
+
+solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]);
