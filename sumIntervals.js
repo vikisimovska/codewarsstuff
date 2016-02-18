53525dd5047f73eec6ed874eebf727296b175402
vokoshyv <ronsuij@gmail.com>
2016-02-18 14:36:00 -0800

    (feat) initializes file for sumIntervals.js

---
+++ sumIntervals.js
@@ -0,0 +1,19 @@
+//http://www.codewars.com/kata/52b7ed099cdc285c300001cd/train/javascript
+
+function sumIntervals(intervals){
+  var work = [];
+  intervals = intervals.sort(function(a,b){return a[0]-b[0]});
+  console.log(intervals);
+
+  var combine = function(inter1, inter2){
+
+  }
+
+  var count = 1;
+
+  while (count !== 0;){
+    count = 0;
+  }
+}
+
+
