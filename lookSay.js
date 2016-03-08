701cd4b51157e8cb52339b8d9131eea957a0be0e
vokoshyv <ronsuij@gmail.com>
2016-03-07 16:57:42 -0800

    (feat) implements solution for lookSay.js

---
+++ lookSay.js
@@ -0,0 +1,30 @@
+// http://www.codewars.com/kata/530045e3c7c0f4d3420001af/train/javascript
+
+function lookSay(number){
+  var work = number.toString();
+
+  var pointer = 1;
+  var current = work[0];
+  var result = [];
+  var count = 1;
+
+  while (pointer < work.length){
+
+    if (work[pointer] !== current){
+      result.push(count);
+      result.push(current);
+      current = work[pointer];
+      count = 1;
+    } else {
+      count++;
+    }
+    pointer++;
+  }
+
+  result.push(count);
+  result.push(current);
+  return parseInt(result.join(''));
+}
+
+
+console.log(lookSay(1211));
