7a5811863da29e6024fcacd6b0512b187a26387f
vokoshyv <ronsuij@gmail.com>
2016-03-28 20:54:56 -0700

    (feat) implements regex solution

--- breakCamelCase.js
+++ breakCamelCase.js
@@ -1,23 +1,4 @@
-// http://www.codewars.com/kata/5208f99aee097e6552000148/train/javascript
-
 // complete the function
 function solution(string) {
-  var result = '';
-  var lib = {};
-  var upper = 'QWERTYUIOPASDFGHJKLZXCVBNM';
-  
-  for (var i = 0; i < upper.length; i++){
-    lib[upper[i]] = true;
-  }
-
-  for (var i = 0; i < string.length; i++){
-    if (lib[string[i]]){
-      result += ' ';
-      result += string[i];
-    } else {
-      result += string[i];
-    }
-  }
-
-  return result;
+  return string.replace(/([A-Z])/g, ' $1');
 }
