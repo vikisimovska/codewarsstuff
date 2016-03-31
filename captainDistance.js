1eb324ff9c386dd376ec1efab4a38ec166fabb12
vokoshyv <ronsuij@gmail.com>
2016-03-31 14:43:56 -0700

    (feat) initializes file for captainDistance.js

---
+++ captainDistance.js
@@ -0,0 +1,8 @@
+// http://www.codewars.com/kata/524ada2bdc2121b521000353/train/javascript
+
+// complete the function to calculate the distance between two coordinates.
+// Input: the two coordinates
+// Output: return the distance in kilometers
+function distance(coord1, coord2) {
+
+}
