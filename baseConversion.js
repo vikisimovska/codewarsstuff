90ace09e15463c10eae2b814a165842d1f580b04
vokoshyv <ronsuij@gmail.com>
2016-03-31 14:07:13 -0700

    (feat) initializes file for baseConversion.js

---
+++ baseConversion.js
@@ -0,0 +1,14 @@
+// http://www.codewars.com/kata/526a569ca578d7e6e300034e/train/javascript
+
+function convert(input, source, target) {
+  var Alphabet = {
+    BINARY:        '01',
+    OCTAL:         '01234567',
+    DECIMAL:       '0123456789',
+    HEXA_DECIMAL:  '0123456789abcdef',
+    ALPHA_LOWER:   'abcdefghijklmnopqrstuvwxyz',
+    ALPHA_UPPER:   'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    ALPHA:         'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    ALPHA_NUMERIC: '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  };
+}
