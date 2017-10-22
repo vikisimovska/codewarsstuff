d546c8b2325c229ed3c1af03955699c5b47f5e8c
vokoshyv <ronsuij@gmail.com>
2017-10-22 14:29:44 -0700

    (feat) initializes file for equalSidesArray.js

--- artists/index.js
+++ artists/index.js
@@ -6,7 +6,9 @@ var popular_artists = require('./popular_artists.js');
 
 fs.readFile('Artist_lists_small.txt', 'utf8', (err, data) => {
   if (err) throw err;
+  console.time('Time needed to run: ');
   console.log(popular_artists(data));
+  console.timeEnd('Time needed to run: ');
 });
 
 })()
--- artists/popular_artists.js
+++ artists/popular_artists.js
@@ -1,6 +1,57 @@
+(function(){
+'use strict';
+
 function popular_artists(input){
+
+  // this chunk splits up the text file by each line
+  // and then goes on to split up each line by artists
   var lines = input.split('\n');
-  console.log(lines);
+  lines = lines.map((line)=>{
+    return line.split(',');
+  })
+  
+  // creates two hashes that I'll be working with
+  var results = {};
+  var work = {};
+  var key;
+
+  // iterates through each line
+  // at each line, goes through each author, and combines
+  // with every author already found in that line to create
+  // a key
+  // takes this key and increments or inserts into the 
+  // results hash
+  lines.forEach((line)=>{
+    work = {};
+    line.forEach((artist_1)=>{
+      for (let artist_2 in work){
+        key = [artist_1, artist_2].sort().join('_');
+        results[key]? results[key]++ : results[key] = 1;
+      }
+      work[artist_1] = true;
+    })
+  })
+
+  // deletes all entries in results that have a count 
+  // that is less than 50
+  for (let key in results){
+    if (results[key] < 50){
+      delete results[key];
+    }
+  }
+
+  return results;
+}
+
+function popular_artists_2(input){
+  var lines = input.split('\n');
+  lines = lines.map((line)=>{
+    return line.split(',');
+  })
+
+  
 }
 
 module.exports = popular_artists;
+
+})();
