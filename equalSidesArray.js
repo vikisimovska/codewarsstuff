52f6a85d8990969ded3e0f1e653f46bfd295f302
vokoshyv <ronsuij@gmail.com>
2017-10-22 14:39:20 -0700

    (feat) implement solution for equalSidesArray.js

--- equalSidesArray.js
+++ equalSidesArray.js
@@ -1,4 +1,23 @@
-function findEvenIndex(arr)
-{
-  //Code goes here!
+function findEvenIndex(arr){
+  let total = 0;
+  arr.forEach(item=>{
+    total += item;
+  })
+
+  if (arr.length === 1){
+    return 0;
+  }
+
+  let left = 0;
+  let right = total;
+
+  for (let i = 0; i < arr.length; i++){
+    right -= arr[i];
+    if (left === right){
+      return i;
+    }
+    left += arr[i];
+  }
+
+  return -1;
 }
